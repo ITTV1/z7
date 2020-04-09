3301b2a38d2d61597bbc35f6bdd77c15b37e108f
ITTV1 <62187261+ITTV1@users.noreply.github.com>
2020-04-09 07:27:35 +0300

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,45 @@
+<?php
+
+Тригонометрические функции
+
+
+double pi()
+
+M_PI
+
+float deg2rad(float $degs)
+$degs/180*M_PI
+
+M_PI = 3.14
+
+
+float rad2deg(float $degs)
+
+
+
+float cos(float $args)
+
+
+
+float asin(float $args)
+
+
+float atan(float $args)
+
+float atan2(float $y, float $x)
+
+$a = atan2(1,1)
+
+
+
+float sin(float $radian)
+
+
+float cos(float $radian)
+
+
+
+float tan(float $radian)
+
+
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+Степенные функции
+
+float sqrt(float $args)
+
+NAN
+
+
+float log(float $args)
+NAN
+-Infinite
++Infinite
+
+float exp(float $args)
+
+e(2.71828182....)
+
+float pow(float $b, float $args)
+NAN
+-Infinite
++Infinite
+?>
